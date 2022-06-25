5beb32f549156c6fdbd25c9636de659001ae1515
devemem <udohememisong@gmail.com>
2022-06-25 07:01:30 +0100

    details JS

--- zuri.js
+++ zuri.js
@@ -1,7 +1,12 @@
-let details = {
-    name: 'Emem',
-    height: 170.18,
-    country: 'Nigeria',
-}
+// const details = {
+//     name: 'Name: Emem Udoh',
+//     height: 'Height: 5ft 7inches',
+//     country: 'Country: Nigeria',
+// }
 
-console.log(details);
+// console.log(details.name);
+// console.log(details.height);
+// console.log(details.country);
+document.write ("Name: Emem Udoh");
+ document.write ("<br>" + "Height: 5ft 7inches");
+  document.write ("<br>" + "Country: Nigeria");
